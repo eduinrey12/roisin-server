06e70ad18370cad46834b1647a798974760dee5b
Eduin Rey <eduinrey12@gmail.com>
2026-07-29 02:12:30 -0500

    chore: enable CORS for all origins temporarily

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: true, // Allow any origin for now, later we restrict to the Vercel URL
     credentials: true,
   });
 
